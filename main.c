81ed987e60aff484974028d13bc4fd93f73b742f
Harrison Travers <Harrison.d.travers@gmail.com>
2020-02-28 16:46:08 +0800

    Made program better

--- main.c
+++ main.c
@@ -1,16 +1,18 @@
 #include<stdio.h>
 void GetName(char*);
+void GetAge(int*);
 void Hello();
-void HelloWorldFormat(char* name);
+void HelloWorldFormat(char* name, int age);
 
 int main(void)
 {
-    char name[20];
-    GetName(name);
-    HelloWorldFormat(name);
+    char name[40];
+	int age;
+    GetName(firstName);
+	GetAge(&age);
+    HelloWorldFormat(name, age);
     return 0;
 }
-
 
 void Hello()
 {
@@ -19,12 +21,24 @@ void Hello()
 
 void GetName(char *name)
 {
-    printf("Enter Name: ");
+    printf("Enter Full Name: ");
     scanf("%s",name);
+}
+
+void GetAge(int *age)
+{
+    printf("Enter Full Age: ");
+    scanf("%d",age);
 }
 
 void HelloWorldFormat(char* name)
 {
     Hello();
     printf("My name is %s\n",name);
+}
+
+void HelloWorldFormat(char* name, int age)
+{
+    Hello();
+    printf("My name is %s and I am %d years old.\n", name, age);
 }
